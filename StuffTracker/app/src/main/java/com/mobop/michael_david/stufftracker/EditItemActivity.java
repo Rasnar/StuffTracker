c5335ffcffbe2a306c6601bd3ea8885a568209ca
Michaël Polla <michaelpolla@hotmail.com>
2016-10-23 19:16:33 +0200

    Add EditItem and InfoItem activities.

---
+++ StuffTracker/app/src/main/java/com/mobop/michael_david/stufftracker/EditItemActivity.java
@@ -0,0 +1,13 @@
+package com.mobop.michael_david.stufftracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+    }
+}
