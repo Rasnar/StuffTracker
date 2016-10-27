cc3e10835a8d922f5f2374fe649d8b83d03b7840
Michaël Polla <michaelpolla@hotmail.com>
2016-10-27 18:33:56 +0200

    Add onNewIntent method, for working with NFC events.

--- StuffTracker/app/src/main/java/com/mobop/michael_david/stufftracker/MainActivity.java
+++ StuffTracker/app/src/main/java/com/mobop/michael_david/stufftracker/MainActivity.java
@@ -1,13 +1,27 @@
 package com.mobop.michael_david.stufftracker;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NfcBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when a new intent occurs. In our case, we check for NFC intents.
+     * @param intent
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if ((intent != null) && (intent.getAction() != null) && (intent.getAction().contains("android.nfc"))) {
+            Tag tagData = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
     }
 }
