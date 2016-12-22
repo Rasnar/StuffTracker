da91b14be820e15c07096fc90374eba56db00ba4
Michaël Polla <michaelpolla@hotmail.com>
2016-12-22 01:05:59 +0100

    Create a StuffItem from all items in database (using only the name for now).

--- app/src/main/java/com/mobop/michael_david/stufftracker/MainActivity.java
+++ app/src/main/java/com/mobop/michael_david/stufftracker/MainActivity.java
@@ -111,13 +111,15 @@ public class MainActivity extends NfcBaseActivity implements
         Toast.makeText(this, "Number of items : " + cursor.getCount(), Toast.LENGTH_SHORT).show();
 
         if(cursor.moveToFirst()) {    // Result(s) found;
-            stuffTrackerManager.addStuffItem(new StuffItem(BitmapFactory.decodeResource(getResources(),
-                    R.drawable.default_photo), cursor.getString(cursor.getColumnIndexOrThrow(DBHandler.COLUMN_NAME)),
-                    "description",
-                    "categories",
-                    "",
-                    null,
-                    null));
+            while (cursor.moveToNext()) {
+                stuffTrackerManager.addStuffItem(new StuffItem(BitmapFactory.decodeResource(getResources(),
+                        R.drawable.default_photo), cursor.getString(cursor.getColumnIndexOrThrow(DBHandler.COLUMN_NAME)),
+                        "description",
+                        "categories",
+                        "",
+                        null,
+                        null));
+            }
         }
         else {
             Toast.makeText(this, "Aucun objet enregistré.", Toast.LENGTH_LONG).show();
