45add637067a40e22a81e33f7916b86eda8a7318
Michaël Polla <michaelpolla@hotmail.com>
2017-01-04 16:18:34 +0100

    Added a method to generate a filename based on the current date and time.

--- app/src/main/java/com/mobop/michael_david/stufftracker/StringUtils.java
+++ app/src/main/java/com/mobop/michael_david/stufftracker/StringUtils.java
@@ -1,5 +1,8 @@
 package com.mobop.michael_david.stufftracker;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Utility classes for String manipulations.
  */
@@ -16,5 +19,17 @@ public class StringUtils {
             builder.append(String.format("%02x", b));
         }
         return builder.toString();
+    }
+
+    /**
+     * Creates a filename based on the current date and time and with the extension provided.
+     * Example : 2017-01-02-15-05-02.jpg
+     * @param fileExt The file extension with separator, like : .jpg
+     * @return The filename created.
+     */
+    public static String getDateTimeFilename(String fileExt) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String timestamp = dateFormat.format(new Date());
+        return timestamp + fileExt;
     }
 }
